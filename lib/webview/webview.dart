a9b61ed7c57a1c7a049bed9dcbcf34be833b1e81
halfmoon-mind <5338095@gmail.com>
2024-03-19 09:26:54 +0900

    🧨 feat: remove linux and window, macos file

--- lib/webview/webview.dart
+++ lib/webview/webview.dart
@@ -2,6 +2,26 @@ import 'package:flutter/material.dart';
 import 'package:flutter_event_presentation/local/testview.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
 
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
 
